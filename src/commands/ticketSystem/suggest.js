a986168ae4d34389793ac3dadcded5e9cd7d3060
tolliam <etienne.maillot@etu.imt-nord-europe.fr>
2023-11-07 17:54:42 +0100

    suggest cmd modification

--- src/commands/ticketSystem/suggest.js
+++ src/commands/ticketSystem/suggest.js
@@ -17,22 +17,6 @@ const {
      name: 'suggest',
      description: 'participe activement à l\'amélioration du bot. Soumets tes idées et suggestions directement au développeur !',
      type: 1,
-     options:[
-         {
-             name: 'channel',
-             description: 'Le channel dans lequel tu souhaite mettre le system de tickets',
-             type: ApplicationCommandOptionType.Channel,
-             required: true,
- 
-         },
-         {
-             name: 'category',
-             description: 'La catégorie dans lequel tu souhaite envoyer les tickets créé',
-             type: ApplicationCommandOptionType.Channel,
-             required: true,
-         },
-     ],
-     permissionsRequired: [PermissionFlagsBits.Administrator],
  
      callback: async (client, interaction) => {
         interaction.reply({content:'Cette commande est en cours de developpement', ephemeral: true} )
