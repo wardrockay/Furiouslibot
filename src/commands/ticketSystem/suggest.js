2d913a2628840435e576c81024919ba80cffe751
tolliam <etienne.maillot@etu.imt-nord-europe.fr>
2023-11-07 13:56:55 +0100

    init cmd suggest

---
+++ src/commands/ticketSystem/suggest.js
@@ -0,0 +1,41 @@
+const {
+    EmbedBuilder,
+    ActionRowBuilder,
+    StringSelectMenuBuilder,
+    ApplicationCommandOptionType,
+    PermissionFlagsBits,
+   } = require('discord.js');
+ const mongoose = require('mongoose');
+ const ticketSchema = require('../../schema/ticketSchema');
+ 
+ module.exports = {
+     /**
+    * @param {Client} client
+    * @param {Interaction} interaction
+    */
+ 
+     name: 'suggest',
+     description: 'participe activement à l\'amélioration du bot. Soumets tes idées et suggestions directement au développeur !',
+     type: 1,
+     options:[
+         {
+             name: 'channel',
+             description: 'Le channel dans lequel tu souhaite mettre le system de tickets',
+             type: ApplicationCommandOptionType.Channel,
+             required: true,
+ 
+         },
+         {
+             name: 'category',
+             description: 'La catégorie dans lequel tu souhaite envoyer les tickets créé',
+             type: ApplicationCommandOptionType.Channel,
+             required: true,
+         },
+     ],
+     permissionsRequired: [PermissionFlagsBits.Administrator],
+ 
+     callback: async (client, interaction) => {
+ 
+         
+     }
+ };
