8fb9216fdff26920920ca474d6739f21ffcf1ee6
Etienne Maillot <tolliam.e+gitbash@gmail.com>
2023-11-07 07:51:28 +0100

    Send message
    When user close ticket, Dm is sent

--- src/messageComponents/ticketSystem/ticketDeleteBtn.js
+++ src/messageComponents/ticketSystem/ticketDeleteBtn.js
@@ -1,3 +1,6 @@
+const {
+  EmbedBuilder,
+} = require('discord.js');
 const ticketSchema = require('../../schema/ticketSchema')
 
 module.exports = {
@@ -10,5 +13,16 @@ module.exports = {
   callback: async (client, interaction) => {
     console.log('delete btn pressed')
     await interaction.channel.delete().catch(err => {});
+
+    const dmEmbed = new EmbedBuilder()
+    .setTitle(`Ton ticket a été fermé`)
+    .setDescription('Merci de nous avoir contacté. Si tu as besoin de quoi que se soit d\'autre, n\'hésite pas à creer un nouveau ticket.')
+    .setFooter({text: `ticket dans ${interaction.guild.name}`})
+    .setTimestamp()
+
+    await interaction.member.send({embeds: [dmEmbed]}).catch(err => {
+      return;
+    })
+
   }
 }
