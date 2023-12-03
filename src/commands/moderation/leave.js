979eef80db503abbbe563515c9719c5baf2b037b
Etienne Maillot <tolliam.e+gitbash@gmail.com>
2023-12-03 13:17:26 +0100

    modif

--- src/commands/moderation/leave.js
+++ src/commands/moderation/leave.js
@@ -1,34 +1,34 @@
 const {
     ApplicationCommandOptionType,
     PermissionFlagsBits,
-  } = require('discord.js');
+} = require('discord.js');
 
-  const config = require('../../../config.json');
-  
-  module.exports = {
+const config = require('../../../config.json');
+
+module.exports = {
     /**
      *
      * @param {Client} client
      * @param {Interaction} interaction
      */
-  
+
     name: 'leave',
     description: 'Make your bot leave a guild.',
     options: [
-      {
-        name: 'target-guild',
-        description: 'The guild you want to ban.',
-        type: ApplicationCommandOptionType.String,
-        required: true,
-      }
+        {
+            name: 'target-guild',
+            description: 'The guild you want to ban.',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        }
     ],
-  
-    callback: async (client, interaction) => {
-      const targetGuildId = interaction.options.get('target-guild').value;
 
-      await interaction.deferReply();
-  
-      const targetGuild = await interaction.guilds.cache.get(targetGuildId);
+    callback: async (client, interaction) => {
+        const targetGuildId = interaction.options.get('target-guild').value;
+
+        await interaction.deferReply();
+
+        const targetGuild = await client.guilds.cache.get(targetGuildId);
 
         if (!targetGuild) {
             await interaction.editReply("That guild doesn't exist.");
@@ -43,24 +43,18 @@ const {
         const guildsInfo = client.guilds.cache.map(guild => ({
             id: guild.id,
             name: guild.name
-          }));
+        }));
 
         const guildInfo = guildsInfo.find(guildInfo => guildInfo.id === targetGuildId);
         if (!guildInfo) {
             console.log(`Left guild ${targetGuild.name} (ID: ${targetGuild.id}).`);
             return;
-        }else{
+        } else {
             console.log(`Failed to leave guild ${targetGuild.name} (ID: ${targetGuild.id}).`);
             return;
         }
-  
-
-  
- 
-  
 
     },
-  
-  
-  };
-  
+
+
+};
